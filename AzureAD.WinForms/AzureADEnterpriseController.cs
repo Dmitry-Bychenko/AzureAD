a29614f394aa37423b2154729d295ef94e711360
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-08-11 00:14:11 +0300

    Enterprise Controller

---
+++ AzureAD.WinForms/AzureADEnterpriseController.cs
@@ -0,0 +1,43 @@
+﻿using AzureAD;
+using AzureAD.Structure;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAD.WinForms {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// Enterprise Controller
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static class AzureADEnterpriseController {
+    #region Public
+
+    /// <summary>
+    /// Create and Load Enterprise
+    /// </summary>
+    public static async Task<AzureEnterprise> LoadAsync(string application,
+                                                        string login,
+                                                        string password,
+                                                        string tenant,
+                                                        IEnumerable<string> permissions) {
+      var connection = AzureADConnectionController.Connect(
+        application, login, password, tenant, permissions);
+
+      if (connection is null || !connection.IsConnected)
+        return null;
+
+      return await AzureEnterprise.Create(connection);
+    }
+
+    #endregion Public
+  }
+
+}
--- AzureADExperimentsForm/MainForm.cs
+++ AzureADExperimentsForm/MainForm.cs
@@ -35,40 +35,30 @@ namespace AzureADExperimentsForm {
 
     #region Algorithm
 
-    private bool CoreConnect() {
-      if (m_Connection is not null && m_Connection.IsConnected)
-        return true;
+    private void CoreConnect() {
+      lbProgress.Visible = true;
+      lbProgress.Text = "Loading...";
 
-      m_Connection = AzureADConnectionController.Connect(
+      m_EnterpriseTask = AzureADEnterpriseController.LoadAsync(
         "d2d7067e-15ac-4f1e-8130-75bc4a6ce717",
         "sync_aad_1c@nedra.digital",
         "Har15204",
         "1b4a1891-24bd-451e-8548-48986af6f553", 
-        new string[] { });
-
-      if (m_Connection is null || !m_Connection.IsConnected)
-        return false;
-
-      lbProgress.Text = "Loading...";
-      lbProgress.Visible = true;
-
-      m_EnterpriseTask = AzureEnterprise.Create(m_Connection);
-
-      return true;
+        new string[] { "User.Read.All" });
     }
 
     private async Task CoreRun() {
-      if (!CoreConnect()) {
-        rtbMain.Text = "Not Connected...";
-
-        return;
-      }
+      CoreConnect();
 
       m_Enterpise = await m_EnterpriseTask;
 
       lbProgress.Visible = false;
 
-      rtbMain.Text = "Connected & Loaded";
+      if (m_Enterpise is null) {
+        rtbMain.Text = "Failed to Load";
+
+        return;
+      }
 
       var result = m_Enterpise
         .Users
