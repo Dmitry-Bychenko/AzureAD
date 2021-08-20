98d278e38bc57b69b7f3bd1fe781c685df830182
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-08-21 00:14:19 +0300

    AzureImport

---
+++ AzureAD/Structure/AzureImport.User.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Graph;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AzureAD.Structure {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class AzureImportUser {
+    #region Private Data
+    #endregion Private Data
+
+    #region Algorithm
+
+    private static User CloneUser(User source) {
+      if (source is null)
+        return null;
+
+      User result = new User();
+
+      result.Id = source.Id;
+      result.UserPrincipalName = source.UserPrincipalName;
+
+      var properties = typeof(User)
+        .GetProperties()
+        .Where(p => p.DeclaringType == typeof(User))
+        .Where(p => p.CanWrite && p.CanRead)
+        .Where(p => !p.GetGetMethod().IsStatic)
+        .Where(p => p.GetGetMethod().IsPublic);
+
+      foreach (PropertyInfo prop in properties) {
+        object value = prop.GetValue(source);
+
+        // if (value is IEnumerable<string> array)
+        //   value = array.ToArray();
+
+        prop.SetValue(result, value);
+      }
+
+      foreach (Extension sourceExtension in source.Extensions) {
+        OpenTypeExtension resultExtension = new OpenTypeExtension() {
+          Id = sourceExtension.Id,
+          ExtensionName = sourceExtension.Id,
+        };
+
+        Dictionary<string, object> dict = new Dictionary<string, object>();
+
+        foreach (var pair in sourceExtension.AdditionalData)
+          dict.Add(pair.Key, pair.Value);
+
+        resultExtension.AdditionalData = dict;
+
+        result.Extensions.Add(resultExtension);
+      }
+
+      return result;
+    }
+
+    #endregion Algorithm
+
+    #region Create
+
+    internal AzureImportUser(AzureImport import, User user) {
+      Import = import ?? throw new ArgumentNullException(nameof(import));
+
+      User = CloneUser(user);
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Import Kind
+    /// </summary>
+    public AzureImportKind ImportKind { get; set; } = AzureImportKind.Garbage;
+
+    /// <summary>
+    /// Import
+    /// </summary>
+    public AzureImport Import { get; }
+
+    /// <summary>
+    /// Enterprise
+    /// </summary>
+    public AzureEnterprise Enterprise => Import.Enterprise;
+
+    /// <summary>
+    /// Graph Client
+    /// </summary>
+    public GraphServiceClient GraphClient => Import.Enterprise.GraphClient;
+
+    /// <summary>
+    /// User
+    /// </summary>
+    public User User { get; }
+
+    /// <summary>
+    /// Source User
+    /// </summary>
+    public User SourceUser => User is null
+      ? null
+      : Enterprise.FindUser(User.Id)?.User;
+
+    #endregion Public
+  }
+
+}
---
+++ AzureAD/Structure/AzureImport.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Graph;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAD.Structure {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public enum AzureImportKind {
+    None = 0,
+    Insert = 1,
+    Delete = 2,
+    Update = 3,
+    Garbage = 4,
+  }
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public sealed class AzureImport {
+    #region Private Data
+
+    private List<AzureImportUser> m_Items = new List<AzureImportUser>();
+
+    private Dictionary<string, AzureImportUser> m_Dictionary = new(StringComparer.OrdinalIgnoreCase);
+
+    #endregion Private Data
+
+    #region Algorithm
+    #endregion Algorithm
+
+    #region Create
+
+    /// <summary>
+    /// Standard Constructor
+    /// </summary>
+    public AzureImport(AzureEnterprise enterprise) {
+      Enterprise = enterprise ?? throw new ArgumentNullException(nameof(enterprise));
+
+      foreach (var user in Enterprise.Users) {
+        var item = new AzureImportUser(this, user.User);
+
+        m_Items.Add(item);
+
+        m_Dictionary.Add(item.User.Id, item);
+        m_Dictionary.Add(item.User.UserPrincipalName, item);
+      }
+
+      foreach (var item in m_Items) {
+        var manager = item.SourceUser?.Manager;
+
+        if (manager is not null)
+          item.User.Manager = Find(manager.Id).User;
+      }
+    }
+
+    #endregion Create
+
+    #region Public
+
+    /// <summary>
+    /// Enterprise
+    /// </summary>
+    public AzureEnterprise Enterprise { get; }
+
+    /// <summary>
+    /// Graph Client
+    /// </summary>
+    public GraphServiceClient GraphClient => Enterprise.Connection;
+
+    /// <summary>
+    /// Items
+    /// </summary>
+    public IReadOnlyList<AzureImportUser> Items => m_Items;
+
+    /// <summary>
+    /// Find
+    /// </summary>
+    public AzureImportUser Find(string id) {
+      if (string.IsNullOrEmpty(id))
+        return null;
+
+      return m_Dictionary.TryGetValue(id, out var result) ? result : null;
+    }
+
+    /// <summary>
+    /// Add
+    /// </summary>
+    public AzureImportUser Add() {
+      AzureImportUser result = new AzureImportUser(this, new User());
+
+      result.ImportKind = AzureImportKind.Insert;
+      result.User.Id = Guid.NewGuid().ToString();
+
+      return result;
+    }
+
+    #endregion Public
+  }
+
+}
